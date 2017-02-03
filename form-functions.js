d9b80b2ff5bab11484ed6312b79641a8ac6c32a1
RBroden <robertbroden@outlook.com>
2017-02-03 16:35:02 -0500

    add tags to analyzed documents

---
+++ form-functions.js
@@ -0,0 +1,193 @@
+const renderer = require('./renderer.js')
+renderer.test("hi")
+/*
+ * Use select element #inputType to change view of input
+*/
+// assign select element #inputType to variable
+let selectInputType = document.getElementById("inputType");
+// assign input element #inputText to variable
+let inputTextArea = document.getElementById("inputTextArea");
+let inputText = document.getElementById("inputText");
+// assign input element #inputUrl to variable
+let inputUrlArea = document.getElementById("inputUrlArea");
+// assign input element #inputDocument to variable
+let inputDocumentArea = document.getElementById("inputDocumentArea");
+// array of results from analyzing documents
+let analyzedDocuments = [];
+let commonWords = [];
+
+
+// event listener for select #inputType onchange
+selectInputType.addEventListener("change", function(){
+
+  // display value for hiding elements
+  let hideStyle = "none";
+  let showStyle = "block";
+
+  // change #inputText display to hideStyle
+  inputTextArea.style.display = hideStyle;
+  // change #inputUrl display to hideStyle
+  inputUrlArea.style.display = hideStyle;
+  // change #inputDocument display to hideStyle
+  inputDocumentArea.style.display = hideStyle;
+
+  switch(selectInputType.value){
+    case 'text':
+      inputTextArea.style.display = showStyle;
+      break;
+
+    case 'url':
+      inputUrlArea.style.display = showStyle;
+      break;
+
+    case 'document':
+      inputDocumentArea.style.display = showStyle;
+      break;
+  }
+});
+
+// form submission
+function onSubmit(){
+
+  switch(selectInputType.value){
+    case 'text':
+      processText();
+      break;
+
+    case 'url':
+      //console.log("analyze url");
+      break;
+
+    case 'document':
+      //console.log("analyze document");
+      break;
+  }
+
+  //console.log(analyzedDocuments);
+
+  commonWords = findCommonWords();
+
+  console.log(commonWords);
+
+  // keeps form from reloading page
+  return false;
+  // end onSubmit
+}
+
+// process textarea input
+function processText(){
+  let analyzedDocument = {
+    words: [],
+    wordCount: 0,
+    tags: []
+  };
+  // assign value of input #inputText to variable
+  let text = inputText.value.toLowerCase();
+  // split text into sentences by .,!, and ?
+  let sentences = text.split(/[\.,!\?]+/);
+  // iterate sentences to create results
+  for(let sentence of sentences){
+    // split sentence into words
+    let sentenceWords = sentence.split(' ');
+    // itereate over words
+    for(let word of sentenceWords){
+      // update analyzedDocument words array
+      // NEEDS SANITIZATION
+      updateWords(word);
+      // end of iterate words
+    }
+    // end of iterate sentences
+  }
+
+  // sort words descending by count
+  analyzedDocument.words.sort((a,b)=>{
+    return parseInt(b.count) - parseInt(a.count);
+  });
+
+  // Populate tags array
+  if(commonWords.length > 0){
+    let tagCount = 0;
+    loop1:
+    for(let i=0; i < analyzedDocument.words.length; ++i){
+      //let tagAdd = false;
+      if(analyzedDocument.tags.length == 5){
+        break loop1;
+      }
+      loop2:
+      for(let j=0; j< commonWords.length; ++j){
+        if(analyzedDocument.words[i].value ==
+          commonWords[j].value){
+          console.log("fount");
+          //tagAdd = true;
+          //break loop2;
+          continue loop1;
+        }
+      }
+      //if(!tagAdd){
+        analyzedDocument.tags[tagCount] =
+        analyzedDocument.words[i].value;
+        ++tagCount;
+      //}
+    //iterate over words and search if !exists in commonWords
+    //if it does not exist in commonWords add to tags
+    }
+  }
+
+  console.log(analyzedDocument);
+
+  // push analyzedDocument to analyzedDocuments
+  analyzedDocuments.push(analyzedDocument);
+
+  // search for word in array words
+  function updateWords(word){
+    analyzedDocument.wordCount++;
+    for(let i = 0; i < analyzedDocument.words.length; i++){
+      if(analyzedDocument.words[i].value == word){
+        analyzedDocument.words[i].count++;
+        return true;
+      }
+    }
+    analyzedDocument.words.push(createWordObject(word));
+  }
+  // edit word
+  // end of processText
+}
+
+
+function findCommonWords(){
+  let words = [];
+  let wordCount = 0;
+  let limit = 50;
+  for(let document of analyzedDocuments){
+    wordCount += document.wordCount;
+    for(let wordObject of document.words){
+      updateCommonWords(wordObject);
+    }
+  }
+
+  words.sort((a,b)=>{
+    return parseInt(b.count) - parseInt(a.count);
+  });
+
+  //console.log('words');
+  //console.log(words);
+
+  return words.slice(0,limit);
+
+  // redundant to function in processText except the array
+  // possibly create higher scope function with word and array argument
+  function updateCommonWords(wordObject){
+    for(let i = 0; i < words.length; i++){
+      if(words[i].value == wordObject.value){
+        words[i].count++;
+        return true;
+      }
+    }
+    // the parse(stringify()) creates a deep clone
+    words.push(JSON.parse(JSON.stringify(wordObject)));
+  }
+}
+
+function createWordObject(word){
+  return {value: word, count: 1};
+}
--- renderer.js
+++ renderer.js
@@ -2,12 +2,27 @@
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
 
+
+
+// file system module
 const fs = require('fs')
 
+
+// read directory /share. returns array of files named files
 fs.readdir("./share",(err,files)=>{
   console.log(files)
+  // assign element #fileList to variable
   var list = document.getElementById("fileList");
+
+  // iterate through files array
   for(let file of files){
+    // output file name to element #fileList
     list.innerHTML += "<li>"+file+"</li>";
   }
+
+  // end fs.readdir
 });
+
+exports.test = function(message){
+  console.log("test"+message);
+};
